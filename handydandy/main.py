9cce483b0760e0e5a8f78a5787b6060641f8f74c
lucasammer <>
2021-05-21 15:24:16 +0000

    fixes

--- handydandy/main.py
+++ handydandy/main.py
@@ -12,25 +12,26 @@ class colors:
   class replonly:
     def printc(text, color):
       print(f'{color}{text}{colors.ENDC}')
-def asksq(question, defaultanswer):
-  x = input(f'{question}:\n({defaultanswer}) ')
-  if x == "":
-    y = defaultanswer
-  else:
-    y = x
-  return(y)
-def askstandardquestion(question, defaultanswer):
-  asksq(question, defaultanswer)
-def pause():
-  input("press enter to continue\n")
-def quitpause(reason):
-  input("press enter to quit\n")
-  quit(reason)
-def quitask(reason):
-  quitaskv1 = input.asksq("do you want to quit:  (y/n)", "y")
-  if quitaskv1 == "y":
+class input:
+  def askda(question, defaultanswer):
+    x = input(f'{question}:\n({defaultanswer}) ')
+    if x == "":
+      y = defaultanswer
+    else:
+      y = x
+    return(y)
+  def askstandardquestion(question, defaultanswer):
+    input.askda(question, defaultanswer)
+  def pause():
+    input("press enter to continue\n")
+  def quitpause(reason):
+    input("press enter to quit\n")
     quit(reason)
-  else:
-    return()
-def ask(question):
-  input(question)
+  def quitask(reason):
+    quitaskv1 = input.asksq("do you want to quit:  (y/n)", "y")
+    if quitaskv1 == "y":
+      quit(reason)
+    else:
+      return()
+  def ask(question):
+    input(question)
