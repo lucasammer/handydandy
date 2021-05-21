23339f76a76b72c006139408560586c7a172c566
lucasammer <>
2021-05-21 15:15:59 +0000

    updated to [alpha] 0.0.3

--- handydandy/__init__.py
+++ handydandy/__init__.py
@@ -1,3 +1,1 @@
 from handydandy.main import handydandy
-from handydandy.main import handydandy
-from handydandy.main import handydandy.input
--- handydandy/main.py
+++ handydandy/main.py
@@ -1,4 +1,5 @@
-class handydandy:
+
+class colors:
   HEADER = '\033[95m'
   OKBLUE = '\033[94m'
   OKCYAN = '\033[96m'
@@ -8,27 +9,28 @@ class handydandy:
   ENDC = '\033[0m'
   BOLD = '\033[1m'
   UNDERLINE = '\033[4m'
-  def printc(text, color):
-    print(f'{color}{text}{handydandy.colors.ENDC}')
-  def asksq(question, defaultanswer):
-    x = input(f'{question}:\n({defaultanswer}) ')
-    if x == "":
-      y = defaultanswer
-    else:
-      y = x
-    return(y)
-  def askstandardquestion(question, defaultanswer):
-    handydandy.asksq(question, defaultanswer)
-  def pause():
-    input("press enter to continue\n")
-  def quitpause(reason):
-    input("press enter to quit\n")
+  class replonly:
+    def printc(text, color):
+      print(f'{color}{text}{colors.ENDC}')
+def asksq(question, defaultanswer):
+  x = input(f'{question}:\n({defaultanswer}) ')
+  if x == "":
+    y = defaultanswer
+  else:
+    y = x
+  return(y)
+def askstandardquestion(question, defaultanswer):
+  asksq(question, defaultanswer)
+def pause():
+  input("press enter to continue\n")
+def quitpause(reason):
+  input("press enter to quit\n")
+  quit(reason)
+def quitask(reason):
+  quitaskv1 = input.asksq("do you want to quit:  (y/n)", "y")
+  if quitaskv1 == "y":
     quit(reason)
-  def quitask(reason):
-    quitaskv1 = handydandy.input.asksq("do you want to quit:  (y/n)", "y")
-    if quitaskv1 == "y":
-      quit(reason)
-    else:
-      return()
-  def ask(question):
-    input(question)
+  else:
+    return()
+def ask(question):
+  input(question)
